3f78ee21812cd2e66f579e26d3542cf35795926b
ImmortalIdiot <max.krasulya@yandex.ru>
2026-05-03 15:55:39 +0300

    feat: add task repository

---
+++ domain/src/main/java/io/ii/domain/repository/TaskRepository.kt
@@ -0,0 +1,61 @@
+package io.ii.domain.repository
+
+import io.ii.domain.model.DecompositionParams
+import io.ii.domain.model.Task
+
+/**
+ * Репозиторий задач.
+ *
+ * Определяет контракт для работы с задачами и их декомпозицией.
+ * Реализация должна обеспечить получение данных из различных источников,
+ * а также их сохранение.
+ *
+ * Является абстракцией над источниками данных и используется в слое domain.
+ */
+interface TaskRepository {
+
+    /**
+     * Выполняет декомпозицию задачи.
+     *
+     * На основе исходной задачи и заданных параметров формирует набор подзадач.
+     *
+     * @param task исходная задача
+     * @param params параметры декомпозиции
+     * @return задача с заполненным списком подзадач
+     */
+    suspend fun decomposeTask(
+        task: Task,
+        params: DecompositionParams
+    ): Task
+
+    /**
+     * Загружает историю декомпозированных задач.
+     *
+     * @return список ранее сохранённых задач
+     */
+    suspend fun loadDecompositionHistory(): List<Task>
+
+    /**
+     * Получает задачу по её идентификатору.
+     *
+     * @param id идентификатор задачи
+     * @return найденная задача или null, если задача не существует
+     */
+    suspend fun getTaskById(id: String): Task?
+
+    /**
+     * Сохраняет задачу.
+     *
+     * Используется для обновления существующей задачи.
+     *
+     * @param task задача для сохранения
+     */
+    suspend fun updateTask(task: Task)
+
+    /**
+     * Удаляет задачу по её идентификатору.
+     *
+     * @param id идентификатор задачи
+     */
+    suspend fun deleteTask(id: String)
+}
