55547a610a49ae7e8d63a32ae0c94f525fbda8f7
ImmortalIdiot <max.krasulya@yandex.ru>
2026-05-08 20:23:23 +0300

    feat: trust ru certs

--- data/src/main/java/io/ii/data/di/DataModule.kt
+++ data/src/main/java/io/ii/data/di/DataModule.kt
@@ -2,6 +2,7 @@ package io.ii.data.di
 
 import android.content.Context
 import androidx.room.Room
+import io.ii.data.R
 import io.ii.data.local.task.TaskDatabase
 import io.ii.data.local.task.dao.TaskDao
 import io.ii.data.local.token.AccessTokenStorage
@@ -19,14 +20,23 @@ import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
+import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import org.koin.plugin.module.dsl.single
+import java.security.KeyStore
+import java.security.cert.Certificate
+import java.security.cert.CertificateFactory
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
+import javax.net.ssl.X509TrustManager
 
 val dataModule = module {
     single { provideDatabase(androidContext()) }
     single { provideDao(get()) }
 
-    single { provideApiClient() }
+    single { provideOkHttpClient(androidContext()) }
+    single { provideApiClient(get()) }
     single { GigaChatApi(get()) }
     single { AccessTokenStorage(androidContext()) }
 
@@ -54,8 +64,15 @@ private fun provideRepository(
     tokenStorage = tokenStorage
 )
 
-private fun provideApiClient(): HttpClient {
+private fun provideApiClient(
+    okHttpClient: OkHttpClient
+): HttpClient {
     return HttpClient(OkHttp) {
+
+        engine {
+            preconfigured = okHttpClient
+        }
+
         install(ContentNegotiation) {
             json(
                 Json {
@@ -77,4 +94,52 @@ private fun provideApiClient(): HttpClient {
             accept(ContentType.Application.Json)
         }
     }
+}
+
+private const val CERTIFICATE_FACTORY_INSTANCE = "X.509"
+private const val SSL_PROTOCOL = "TLS"
+
+private fun provideOkHttpClient(
+    context: Context
+) : OkHttpClient {
+
+    val certificateFactory = CertificateFactory.getInstance(CERTIFICATE_FACTORY_INSTANCE)
+
+    val certs = listOf(
+        R.raw.russian_trusted_root_ca,
+        R.raw.russian_trusted_sub_ca
+    ).map { id ->
+        context.resources.openRawResource(id).use { stream ->
+            certificateFactory.generateCertificate(stream)
+        }
+    }
+
+    val keyStore = KeyStore.getInstance(
+        KeyStore.getDefaultType()
+    ).apply {
+        load(null, null)
+
+        certs.forEachIndexed { index, certificate ->
+            setCertificateEntry("cert_$index", certificate)
+        }
+    }
+
+    val trustManagerFactory = TrustManagerFactory.getInstance(
+        TrustManagerFactory.getDefaultAlgorithm()
+    ).apply {
+        init(keyStore)
+    }
+
+    val trustManager = trustManagerFactory.trustManagers
+        .filterIsInstance<X509TrustManager>()
+        .first()
+
+    val sslContext = SSLContext.getInstance(SSL_PROTOCOL).apply {
+        init(null, arrayOf(trustManager), null)
+    }
+
+    return OkHttpClient
+        .Builder()
+        .sslSocketFactory(sslContext.socketFactory, trustManager)
+        .build()
 }
